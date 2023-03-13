45a1d75e6148e9cd39477b4663855894b0361d1f
Kazbek <kazbek.se@gmail.com>
2023-03-14 01:34:07 +0300

    Базовая карта

---
+++ src/BlazorTech.LiteMap/LiteMap.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorTech.LiteMap;
+
+public partial class LiteMap
+{
+    [Parameter] public string? DefaultColor { get; set; }
+    [Parameter] public required IEnumerable<LiteMapRegion> Regions { get; set; }
+}
---
+++ src/BlazorTech.LiteMap/LiteMapRegion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorTech.LiteMap
+{
+    public class LiteMapRegion
+    {
+        public required string Title { get; set; }
+        public required string Path { get; set; }
+        
+        internal bool IsHovered { get; set; } = false;
+    }
+}
