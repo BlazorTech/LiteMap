416b330ffdb8fe655352aedeab5657bbe8c45212
Kazbek <kazbek.se@gmail.com>
2023-03-14 02:42:54 +0300

    Доработка прототипа и демо

--- src/BlazorTech.LiteMap/LiteMap.razor.cs
+++ src/BlazorTech.LiteMap/LiteMap.razor.cs
@@ -1,9 +1,37 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace BlazorTech.LiteMap;
 
 public partial class LiteMap
 {
-    [Parameter] public string? DefaultColor { get; set; }
+    [Parameter] public string HoverColor { get; set; } = "#f5e72dd9";
     [Parameter] public required IEnumerable<LiteMapRegion> Regions { get; set; }
+
+    [Parameter] public EventCallback<LiteMapRegion> OnHoverCallback { get; set; }
+
+    LiteMapRegion? _hoveredRegion { get; set; }
+    double _pageX { get; set; }
+    double _pageY { get; set; }
+
+    void MouseMove(MouseEventArgs e)
+    {
+        _pageX = e.PageX;
+        _pageY = e.PageY + 30;
+    }
+
+    async Task MouseOverAsync(MouseEventArgs e, LiteMapRegion region)
+    {
+        region.IsHovered = true;
+        _pageX = e.PageX;
+        _pageY = e.PageY + 30;
+        _hoveredRegion = region;
+        await OnHoverCallback.InvokeAsync(region);
+    }
+    async Task MouseOutAsync(LiteMapRegion region)
+    {
+        region.IsHovered = false;
+        _hoveredRegion = null;
+        await OnHoverCallback.InvokeAsync(region);
+    }
 }
--- src/BlazorTech.LiteMap/LiteMapRegion.cs
+++ src/BlazorTech.LiteMap/LiteMapRegion.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BlazorTech.LiteMap
+﻿namespace BlazorTech.LiteMap
 {
     public class LiteMapRegion
     {
         public required string Title { get; set; }
         public required string Path { get; set; }
+
+        public string BackgroundColor { get; set; } = "#00000000";
         
         internal bool IsHovered { get; set; } = false;
     }
